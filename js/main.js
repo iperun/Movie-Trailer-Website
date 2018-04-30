f220928714ac8c9b42f2aef75c2f4b7afe4ee812
Ivan Perun <peruni101@gmail.com>
2018-04-30 17:41:38 -0400

    h

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
-// Pause the video when the modal is closed
-$(document).on('click', '.hanging-close, .modal-backdrop, .modal', function(event) {
-  // Remove the src so the player itself gets removed, as this is the only
-  // reliable way to ensure the video stops playing in IE
-  $("#trailer-video-container").empty();
-});
+
 // Start playing the video whenever the trailer modal is opened
 $(document).on('click', '.movie-tile', function(event) {
   var trailerYouTubeId = $(this).attr('data-trailer-youtube-id')
@@ -20,4 +15,11 @@ $(document).ready(function() {
   $('.movie-tile').hide().first().show("fast", function showNext() {
     $(this).next("div").show("fast", showNext);
   });
+});
+
+// Pause the video when the modal is closed
+$(document).on('click', '.hanging-close, .modal-backdrop, .modal', function (event) {
+    // Remove the src so the player itself gets removed, as this is the only
+    // reliable way to ensure the video stops playing in IE
+    $("#trailer-video-container").empty();
 });
